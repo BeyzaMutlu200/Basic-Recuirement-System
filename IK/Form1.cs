5fc87f598732f7febbcba6bb86901c1360c8b3de
BeyzaMutlu200 <azyebultum@gmail.com>
2025-09-01 18:49:16 +0300

    Initial commit of IK project

---
+++ IK/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+                textBoxList = new List<TextBox>();
+
+            // Tasarım arayüzü üzerinden eklenen TextBox'ları List'e ekle
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    textBoxList.Add((TextBox)control);
+                }
+            }
+        }
+        private List<TextBox> textBoxList;
+
+
+       public void button1_Click(object sender, EventArgs e)
+        {
+            
+            information IN = new information("", "", "", "", "");
+
+            // Her bir TextBox'ın içeriğini topla
+            IN.Name_sname = textBox1.Text;
+            IN.Id = textBox2.Text;
+            IN.Mail = textBox3.Text;
+            IN.Telno = textBox4.Text;
+            // Seçilen meslek türünü alın
+            string meslek = comboBox1.SelectedItem.ToString();
+
+            // Alınan bilgileri ilgili dosyaya kaydedin
+
+
+
+
+
+       //     SaveToFile(meslek);      // birde burasııııı
+
+
+
+            // Toplanan verileri SaveToFile metoduna geçir
+            var result = true;
+            if (result)
+            {
+                try
+                {
+                    // Her bir TextBox için kontrolü gerçekleştir
+                    foreach (var textBox in textBoxList)
+                    {
+                        if (string.IsNullOrWhiteSpace(textBox.Text))
+                        {
+                            throw new ArgumentException("doldurlmayan alan/alanlar mevcut !");
+                        }
+
+                        // Diğer işlemleri buraya ekleyebilirsiniz
+                        Console.WriteLine("Girilen Metin: " + textBox.Text);
+
+                    }
+                    if (string.IsNullOrWhiteSpace(label6.Text) || !label6.Text.ToLower().EndsWith(".pdf"))
+                    {
+                        throw new ArgumentException("Geçerli bir PDF dosyası seçilmelidir!");
+                    }
+                    if(string.IsNullOrEmpty(comboBox1.Text))
+                    { throw new ArgumentException("Lütfen meslek seçiniz!"); }
+                }
+                catch (ArgumentException ex)
+                {
+                    // TextBox boşsa burası çalışır
+                    MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                //pdfdosyayolu için nasıl ekleme yapabilrim exception hatasını ?
+            }
+            //     MessageBox.Show("Kayıt İşlemi Başarıyla Gerçekleştirildi ", "Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information); 
+            // ekleyince throw mesajından sonra doğru 
+        }
+        public void button3_Click(object sender, EventArgs e)
+
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select File";
+                openFileDialog.InitialDirectory = @"C:\";
+                openFileDialog.Filter = "PDF Dosyaları|*.pdf|Tüm Dosyalar|*.*";
+                openFileDialog.FilterIndex = 5;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK || openFileDialog.FileName != "")
+                {
+                    // Seçilen PDF dosyasının yolu
+                    string pdfDosyaYolu = openFileDialog.FileName;
+
+                    // PDF dosyasının içeriğini okuma işlemleri burada gerçekleştirilebilir
+
+                    // Örneğin, PDF içeriğini bir TextBox'a yazdırmak
+
+                    label6.Text = pdfDosyaYolu; //DOSYA YOLUNU DOĞRU ALIYORUM AMA DOSYAYA SEÇ YAZISIYLA BİRLİKTE KAYIT İŞLEMİ YAPILIYOR BUNU İSTEMİYORUM NASIL DÜZELTEBİLİRİM. 
+                } 
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ IK/employee.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IK
+{
+
+        class information
+        {
+            public string name_sname;
+            public string id;
+            public string mail;
+            public string telno;
+            public string cv_path;
+
+            public information(string name_sname, string id, string mail, string telno, string cv_path)
+            {
+                this.name_sname = name_sname;
+                this.id = id;
+                this.mail = mail;
+                this.telno = telno;
+                this.cv_path = cv_path;
+            }
+
+            public string Name_sname
+            {
+
+                set
+                {
+                    name_sname = value;
+                    SaveToFile(name_sname);
+
+                }
+            }
+            public string Id
+            {
+
+                set
+                {
+                    if (value.Length == 11)
+
+                    {
+                        id = value;
+                        SaveToFile(id);
+                    }
+
+                }
+
+            }
+            public string Mail
+            {
+                set
+                {
+                    mail = value;
+                    SaveToFile(mail);
+                }
+            }
+            public string Telno
+            {
+                set
+                {
+                    telno = value;
+                    SaveToFile(telno);
+                }
+            }
+            public string Cv_path
+            {
+                set
+                {
+                    try
+                    {
+                        cv_path = value;
+                        SaveToFile(cv_path);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Dosya yolu bulunamiyor" + ex.Message);
+
+                    }
+
+
+                }
+            }
+
+            public void SaveToFile(string data)
+            {
+              //  string fileName = meslek + "Bilgiler.txt";//bunu nasıl düzeltebilrim ??????????????????
+
+                string filePath = @"C:\Users\Beyza\Desktop\IK\cv.txt"; //Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine(data);
+
+                }
+            }
+
+
+        }
+    }
